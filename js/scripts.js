b486f520c760f38a39629bfbc9760717180cec4e
Alex Lillian <flillian@uw.edu>
2025-05-10 12:00:52 -0700

    First COMMIT BABY!!!!

--- js/scripts.js
+++ js/scripts.js
@@ -184,23 +184,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Eileen and Alex's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Jan 15, 2026 17:00'), // TODO change to real date
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Jan 16, 2026 00:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'TBD!',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "So excited to see you at our wedding! For any questions please contact Alex at 858 204 5616."
         }
     });
 
@@ -214,11 +214,22 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== '4b320c8a4b97025a69064d80225a3ef9') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            // var xhr = new XMLHttpRequest();
+            // xhr.open('POST', "https://script.google.com/macros/s/AKfycbz6nTDrUDVzcpbcT6iBhl4MRigF9x-4liony0A1DkIxgTRXXFhb8Zrs6FUHMUltVK2p/exec");
+            // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            // xhr.onreadystatechange = function() {
+            //     console.log(xhr);
+            //     if (xhr.readyState !== 4 || xhr.status !== 200) {
+            //     } else {
+            //         $('#alert-wrapper').html('');
+            //         $('#rsvp-modal').modal('show');
+            //     }
+            // }
+
+            $.post('https://script.google.com/macros/s/AKfycbyudXqlSexNHqXJsGZuVFjcO5yy1OLA_nJmSivVRp7JEIvxBOKJsFkuqBCCvtLdlh-x/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
